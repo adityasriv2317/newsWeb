feeda713514f6e251ff3448f49bc921bde44c277
adityasriv2317 <adityaxia9237@gmail.com>
2024-10-29 23:49:40 +0530

    header setup

--- src/comps/Header.jsx
+++ src/comps/Header.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-        <div className="left, ml-[2vw]">LOGO</div>
+    <div className='header bg-blue-800 text-[2vw] flex text-white justify-between px-[2vw] h-[4vw] shadow-slate-500'>
+
+        <div className="left flex items-center">
+      
+          <div className="logo mr-[2vw] bg-yellow-500 flex items-center cursor-pointer px-[0.8vw] py-0 h-full">
+            LOGO
+          </div>
+          <div className="acc text-[1.5vw] cursor-pointer bg-yellow-500 px-[0.7vw] flex items-center h-full">
+            Account
+          </div>
+        </div>
+      
+        <div className="right, text-[1.8vw] flex items-center">
+          <ul className='list-none gap-[2vw] m-auto flex h-full parent-div'>
+            <li className='cursor-pointer bg-yellow-500 px-[1vw] flex items-center'>Login</li>
+            <li className='cursor-pointer bg-yellow-500 px-[1vw] flex items-center'>SignUp</li>
+          </ul>
+        </div>
+    
     </div>
   )
 }
