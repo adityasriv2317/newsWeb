0c46f47105237f10b6119ead209caaceb35cbf35
adityasriv2317 <adityaxia9237@gmail.com>
2024-10-29 19:43:26 +0530

    git setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './comps/Header'
+
+function App() {
+
+  return (
+    <>
+      <div className='m-0 p-0'>
+        <Header />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/comps/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <div className="left, ml-[2vw]">LOGO</div>
+    </div>
+  )
+}
+
+export default Header
