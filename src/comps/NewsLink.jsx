a9597207e5e62be94fc888d08e7b763a0f7d905c
adityasriv2317 <adityaxia9237@gmail.com>
2024-11-04 22:44:46 +0530

    api change

--- src/comps/NewsLink.jsx
+++ src/comps/NewsLink.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import thumbnail from '/tdb2.png'
 
-const url = "https://newsapi.org/v2/everything?q=the&sortBy=publishedAt&pageSize=50&language=en&apiKey=2ebb187b1dfb4051861d2660ccdf3b12"
+const url = "https://newsapi.org/v2/everything?q=the&sortBy=publishedAt&pageSize=50&language=en&apiKey=79da0ed268ed4145ab0e8967b4c6253b"
 const url3 = '/SampleApi.json'
 
 class NewsLink extends React.Component{
