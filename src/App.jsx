30b4b8d094333f64784c543492c336bf03edebf2
adityasriv2317 <adityaxia9237@gmail.com>
2024-11-04 20:53:46 +0530

    git fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
 import Header from './comps/Header'
 import PageBody from './comps/PageBody'
 import SignupComp from './comps/SignupComp'
 import LoginComp from './comps/LoginComp'
-import AccountComp from './comps/AccountComp'
+import User from './comps/User'
 
 import logo from '/tdb.png'
 let name = "The Daily Bugle"
@@ -13,7 +13,7 @@ let name = "The Daily Bugle"
 function App() {
 
   return (
-    <>
+    <div className='transition-all ease-in-out'>
       {/* router */}
       <Router className='m-0 p-0 font-roboto'>
         {/* header - navigation bar */}
@@ -23,11 +23,11 @@ function App() {
           <Route exact path="/" element={<PageBody />} />
           <Route exact path="/login" element={<LoginComp />} />
           <Route exact path="/signup" element={<SignupComp />} />
-          <Route exact path="/account" element={<AccountComp />} />
+          <Route exact path="/account" element={<User />} />
         </Routes>
       </Router> 
       
-    </>
+    </div>
 
   )
 }
